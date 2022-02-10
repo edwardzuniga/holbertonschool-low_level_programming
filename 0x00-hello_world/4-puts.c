d5a417ac29985f2de3c55f30baf82d7b4290b3ba
Edward Zuniga <4593@holbertonschool.com>
2022-02-10 11:25:26 -0800

    puts-c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
 *main - starting point
 *return - value (0)
 */
 
 int main (void)
 {
-     puts("\"Programming is like building a multilingual puzzle");
-     return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
