bfa993cac3d155e4ca503a98b09bcf0b76ecfb32
Edward Zuniga <4593@holbertonschool.com>
2022-02-11 09:24:08 -0800

    positivo o negativo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,18 +13,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n>o)
-	  {
+	if (n > 0)
+	  {  
 	    ptintf("%s%d\n", n, " is positive");
 	  }
-	else if (n==0)
+	else if (n == 0)
 	  {
 	    printf("%s%d\n", n, " is zero");
 	  }
 	else
 	  {
-	    printf("%s%d\n", n, " is negative")
+	    printf("%s%d\n", n, " is negative");
 	  }
 
 	return (0);
