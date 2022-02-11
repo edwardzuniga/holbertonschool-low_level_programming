4cb9dcb7f8fb159e9efef0f34f9059c1ad251399
Edward Zuniga <4593@holbertonschool.com>
2022-02-11 13:36:38 -0800

    mayuscula y minuscula

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - init
+* Return: value 0
+*/
+
+int main(void)
+{
+char alfa;
+char alfaM;
+for (alfa = 'a'; alpha <= 'z'; alpha++)
+putchar(alfa);
+for (alfaM = 'A'; alfaM <= 'Z'; alfaM++)
+putchar(alfaM);
+putchar('\n')
+return (0);
+}
