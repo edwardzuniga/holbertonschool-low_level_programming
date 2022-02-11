c51171ad6ddfb1c4a278522e691531aaee3d3772
Edward Zuniga <4593@holbertonschool.com>
2022-02-11 11:17:55 -0800

    last-digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ n = n % 10;
 
 if (n > 5)
 printf("%d and is greater than 5\n", n);
-else if (n == 0)
+else if (n ≥ 0)
 printf("%d and is 0\n", n);
 else
 printf("%d and is less than 6 and not 0\n", n);
