b671b6b9c04e6710290d457b1d91cb843edeac3f
Edward Zuniga <4593@holbertonschool.com>
2022-02-11 17:22:32 -0800

    base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Numbers one digit
+* Return: 0
+*/
+int main(void)
+{
+int numero;
+for (numero = '0'; numero < '9'; numero++)
+printf("%d", numero);
+printf("\n");
+return (0);
+}
