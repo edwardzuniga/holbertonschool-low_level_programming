f1d139230c6133172c3509af879dcb2ee454aea2
Edward Zuniga <4593@holbertonschool.com>
2022-02-11 17:27:15 -0800

    numerputchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - init
+* Return: value 0
+*/
+
+int main(void)
+{
+int numero;
+for (numero = '0'; numero <= '9'; numero++)
+{
+putchar(numero);
+}
+putchar('\n');
+return (0);
+}
