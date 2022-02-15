a4ddddc55ffdbf5c4068d34f2c7fd13cdcc51743
Edward Zuniga <4593@holbertonschool.com>
2022-02-15 13:43:46 -0800

    chequeo

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
 * _islower - chequear si es minusculas
+* @c: init
 * Return: value 0
 */
 
 int _islower(int c)
 {
 char chequeo;
-for (chequeo = 'a'; chequeo <= 'z'; chequeo++)  
+for (chequeo = 'a'; chequeo <= 'z'; chequeo++)
 {
 if (c == chequeo)
 return (1);
