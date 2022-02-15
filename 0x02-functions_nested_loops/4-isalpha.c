91902a971111dd2c5be9bf500047dc874a813dc3
Edward Zuniga <4593@holbertonschool.com>
2022-02-15 14:24:01 -0800

    chequeomayusyminus

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _isalpha - chequear si es minusculas o mayusculas
+* @c: init
+* Return: value 0 or 1
+*/
+
+int _isalpha(int c)
+{
+char chequeo;
+char chequeoM;
+for (chequeo = 'a'; chequeo <= 'z'; chequeo++)
+{
+if (c == chequeo)
+return (1);
+}
+for (chequeoM = 'a'; chequeoM <= 'z'; chequeoM++)
+{
+if (c == chequeo)
+return (1);
+}
+return (0);
+}
