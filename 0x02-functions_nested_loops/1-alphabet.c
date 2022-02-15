9521128992318c44384283912838bf0b4d3c3f74
Edward Zuniga <4593@holbertonschool.com>
2022-02-15 13:03:18 -0800

    alfabetominuscula

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - imprimir alfabeto
+* Return: value 0
+*/
+
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+putchar('\n');
+}
