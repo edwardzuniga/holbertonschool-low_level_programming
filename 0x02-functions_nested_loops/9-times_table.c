286f059d0821eb1b39a6dfe5f1bdbc73a1f92726
Edward Zuniga <4593@holbertonschool.com>
2022-02-15 20:27:05 -0800

    tabla9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* times_table - imprime la tabla de multiplicar del 9
+*
+* Return: value 0
+*/
+
+void times_table(void)
+{
+int a;
+int b;
+int c;
+
+for (a = 0; a <= 9; a++)
+{
+_putchar('0');
+for (b = 1; b <= b; b++)
+{
+_putchar(',');
+_putchar(' ');
+
+c = a * b;
+if (c <= 9)
+_putchar(' ');
+else
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+_putchar('\n');
+}
+}
