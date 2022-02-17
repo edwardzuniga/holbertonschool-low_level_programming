9d708d9b0d8497fbed6f50c3e33205a2e3380fbc
Edward Zuniga <4593@holbertonschool.com>
2022-02-17 11:03:36 -0800

    de0a9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - imprimir numeros x10
+* Return: value 0
+*/
+
+void print_numbers(void)
+{
+int numero;
+for (numero = '0'; numero <= '9'; numero++)
+{
+_putchar(numero);
+}
+_putchar('\n');
+}
