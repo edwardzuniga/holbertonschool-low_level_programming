5809b40b4c4864654c5050b975b2d90270ea8482
Edward Zuniga <4593@holbertonschool.com>
2022-02-17 18:29:19 -0800

    100sin

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,15 @@ int x;
 for (x = 1; x < 100; x++)
 if (x % 15 == 0)
 {
-printf("FizzBuzz");
+printf("FizzBuzz ");
 }
 else if (x % 5 == 0)
 {
-printf("Buzz");
+printf("Buzz ");
 }
 else if (x % 3 == 0)
 {
-printf("Fizz");
+printf("Fizz ");
 }
 else
 {
