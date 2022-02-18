003c106df682aa599ec528cda3989a1e3dd7b474
Edward Zuniga <4593@holbertonschool.com>
2022-02-17 18:23:54 -0800

    100sin

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* main - imprime numeros hasta el 100
+* Return: value 0
+*/
+
+int main(void)
+{
+int x;
+for (x = 1; x < 100; x++)
+if (x % 15 == 0)
+{
+printf("FizzBuzz");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz");
+}
+else if (x % 3 == 0)
+{
+printf("Fizz");
+}
+else
+{
+printf("%d ", x);
+}
+printf("Buzz");
+printf("\n");
+return (0);
+}
