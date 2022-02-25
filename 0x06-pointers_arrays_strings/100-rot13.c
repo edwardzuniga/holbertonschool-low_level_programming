cdbf0ff04456b3a7515f23e36b505ce4959c54b7
Edward Zuniga <4593@holbertonschool.com>
2022-02-25 08:53:39 -0800

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* rot13 - invierte el array
+* @str: string main
+* Return: value to str
+*/
+
+char *rot13(char *str)
+{
+int a = 0;
+int b;
+char aport[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char produ[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+while (str[a] != '\0')
+{
+b = 0;
+while (aport[b] != '\0')
+{
+if (str[a] == aport[b])
+{
+str[a] = produ[b];
+}
+b++;
+break;
+}
+a++;
+}
+return (str);
+}
