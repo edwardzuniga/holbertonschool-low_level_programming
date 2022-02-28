c9e04eaf0c40aed32f62e57b76e88b2819082138
Edward Zuniga <4593@holbertonschool.com>
2022-02-28 13:28:34 -0800

    sets the value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* set_string - obtiene la longitud de una subcadena de prefijo
+* @s: puntero doble
+* @to: puntero
+* Return: s + a or value zero
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
