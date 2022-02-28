38521eef23a0dcd38a696c40e8aabc785985dd41
Edward Zuniga <4593@holbertonschool.com>
2022-02-28 09:27:24 -0800

    locatesachar

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: puntero
+* @c: byte constante
+* Return: null
+*/
+
+char *_strchr(char *s, char c)
+{
+int a = 0;
+
+while (s[a] != '\0')
+{
+if (s[a] == c)
+return (s + a);
+a++;
+}
+return ('\0');
+}
