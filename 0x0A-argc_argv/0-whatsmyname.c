8645b907be8b6a7c9eda42feee4c3f8c343c9a51
Edward Zuniga <4593@holbertonschool.com>
2022-03-04 08:43:21 -0800

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - prints its name
+* @argc: tamaño de argv
+* @argv: program
+* Return: value 0
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", *argv);
+return (0);
+}
