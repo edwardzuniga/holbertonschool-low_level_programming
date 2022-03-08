62de119e0e1bfa3ec93a160e51520729ffd91187
Edward Zuniga <4593@holbertonschool.com>
2022-03-08 12:10:50 -0800

    2 dimensional

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,12 +13,7 @@ void free_grid(int **grid, int height)
 {
 int x;
 
-if (grid == NULL || height <= 0)
-{
-return;
-}
-
-for (x = height - 1; x >= 0; x++)
+for (x = 0; x < height; x++)
 {
 free(grid[x]);
 }
