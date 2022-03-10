4d9b03891c967527448bd92b529defdcbd0ebfa8
Edward Zuniga <4593@holbertonschool.com>
2022-03-10 11:41:55 -0800

    concatenar

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* string_ncconcat - allocates memory using malloc
+* @s1: pointer
+* @s2: pointer
+* @n: int
+* Return: value 0
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *str;
+unsigned int ref1, ref2;
+
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+ref1 = 0;
+
+while (s1[ref1] != '\0')
+ref1++;
+str = malloc(sizeof(char) * (ref1 + n + 1));
+if (str == NULL)
+return (NULL);
+ref1 = ref2 = 0;
+while (s1[ref1] != '\0')
+{
+str[ref1] = s1[ref1];
+ref1++;
+}
+while (ref2 < n && s2[ref2] != '\0')
+{
+str[ref1] = s2[ref2];
+ref1++, ref2++;
+}
+str[ref1] = '\0';
+return (str);
+}
