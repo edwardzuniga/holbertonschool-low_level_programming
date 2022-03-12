1a9b6ebc1b3fe539358b89a91adda12ea2025372
Edward Zuniga <4593@holbertonschool.com>
2022-03-11 17:55:25 -0800

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-* *string_ncconcat - concatenates two strings.
+* string_nconcat - concatenates two strings.
 * @s1: pointer
 * @s2: pointer
 * @n: int
@@ -12,31 +12,31 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *str;
-unsigned int in = n, a;
+unsigned int ref1, ref2;
 
 if (s1 == NULL)
 s1 = "";
-
 if (s2 == NULL)
 s2 = "";
 
-for (a = 0; s1[a]; a++)
-in++;
+ref1 = 0;
 
-str = malloc(sizeof(char) * (in + a));
-
+while (s1[ref1] != '\0')
+ref1++;
+str = malloc(sizeof(char) * (ref1 + n + 1));
 if (str == NULL)
 return (NULL);
-
-in = 0;
-
-for (a = 0; s1[a]; a++)
-str[in++] = s1[a];
-
-for (a = 0; s2[a] && a < n; a++)
-str[in++] = s2[a];
-
-str[in] = '\0';
-
+ref1 = ref2 = 0;
+while (s1[ref1] != '\0')
+{
+str[ref1] = s1[ref1];
+ref1++;
+}
+while (ref2 < n && s2[ref2] != '\0')
+{
+str[ref1] = s2[ref2];
+ref1++, ref2++;
+}
+str[ref1] = '\0';
 return (str);
 }
