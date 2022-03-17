e707cbf92b0c7cfa2b6332b160c0d1b6580cc40a
Edward Zuniga <4593@holbertonschool.com>
2022-03-17 13:26:44 -0700

    correct function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,8 @@ op_t ops[] = {
 int i;
 i = 0;
 
-while (i < 5)
-{
-if (*s == *ops[i].op)
-return (*(ops[i]).f);
+while (ops[i].op != NULL && *(ops[i].op) != *s)
 i++;
-}
-return (NULL);
+
+return (ops[i].f);
 }
