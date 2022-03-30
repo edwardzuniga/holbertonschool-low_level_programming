87811244629c6e806f038ffd7965f76422e16b1e
Edward Zuniga <4593@holbertonschool.com>
2022-03-30 16:30:29 -0700

    prints all

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+int current_node = 0;
+
+while (h != NULL)
+{
+if (h->str != NULL)
+{
+printf("[%d] %s\n", h->len, h->str);
+}
+else
+{
+printf("[0] %p\n", h->str);
+}
+h = h->next;
+current_node++;
+}
+return (current_node);
+}
