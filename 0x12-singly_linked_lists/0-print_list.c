633c5cd4591f8d91cb847e56d06ebc3f8b2cb1fa
Edward Zuniga <4593@holbertonschool.com>
2022-03-30 16:47:50 -0700

    prints all

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,13 +15,13 @@ return (0);
 
 while (h != NULL)
 {
-if (h->str != NULL)
+if (h->str == NULL)
 {
-printf("[%d] %s\n", h->len, h->str);
+printf("[%d] %s\n", 0, "(nil)");
 }
 else
 {
-printf("[%d] %s\n", 0, "(nil)");
+printf("[%d] %s\n", h->len, h->str);
 }
 h = h->next;
 current_node++;
