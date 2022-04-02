9fdb725cefaeaa67d7ac74f8d0abbf4ee4d0ff00
Edward Zuniga <4593@holbertonschool.com>
2022-04-01 19:42:04 -0700

    freelist2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  frees a listint_t list.
+ * @head: pointer
+ *
+ * Return: Value 0
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t *liberar;
+
+if (head == NULL)
+{
+return;
+}
+while (head != NULL)
+{
+liberar = head;
+head = liberar->next;
+free(liberar);
+}
+}
