66cacbd2a48045e4e2ad97751ce2634a6e423d62
edwardzuniga <4593@holbertonschool.com>
2022-05-12 16:13:04 -0500

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,8 @@
 #include "lists.h"
 /**
- *
  * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: the pointer
  * Return: number of nodes
- *
  */
 size_t print_dlistint(const dlistint_t *h)
 {
