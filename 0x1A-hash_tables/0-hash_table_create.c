73f2ce0a740cc3ca05a385978ec2796bf0fbaaee
edwardzuniga <4593@holbertonschool.com>
2022-06-06 16:38:29 -0500

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,19 +8,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *n_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-	if (n_table == NULL)
+	if (new_table == NULL)
 	{
-		free(n_table);
+		free(new_table);
 		return (NULL);
 	}
-	n_table->size = size;
-	n_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (n_table->array == NULL)
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
 	{
-		free(n_table);
+		free(new_table);
 		return (NULL);
 	}
-	return (n_table);
+	return (new_table);
 }
