1f18fbedca52ccd7fb8640b20d5e590633afbe59
edwardzuniga <4593@holbertonschool.com>
2022-06-06 16:36:43 -0500

    4. >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,25 +9,24 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *tmp;
-	unsigned int index;
+	hash_node_t *n_tmp;
+	unsigned int i;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	{
-		index = key_index((const unsigned char *)key, ht->size);
-		tmp = ht->array[index];
+		i = key_index((const unsigned char *)key, ht->size);
+		n_tmp = ht->array[i];
 
-		while (ht->array[index])
+		while (ht->array[i])
 		{
-			if (tmp)
+			if (n_tmp)
 			{
-				if (strcmp(tmp->key, (char *)key) == 0)
-					return (tmp->value);
-				tmp = tmp->next;
+				if (strcmp(n_tmp->key, (char *)key) == 0)
+					return (n_tmp->value);
+				n_tmp = n_tmp->next;
 			}
 		}
 	}
 	return (NULL);
 }
-
